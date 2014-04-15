21b2db6b45cb9b962e19bbbefe3d57c6ffa36562
Ceslovas Lopan <l.ceslovas@gmail.com>
2014-04-15 12:01:53 +0200

    Fix unwanted spacing using raster pattern, cleanup

--- dist/paper-node.js
+++ dist/paper-node.js
@@ -9,7 +9,7 @@
  *
  * All rights reserved.
  *
- * Date: Tue Dec 17 22:07:34 2013 +0100
+ * Date: Tue Feb 25 10:49:17 2014 +0100
  *
  ***
  *
@@ -7150,7 +7150,6 @@ var Path = PathItem.extend({
 				if (!compound)
 					this._currentPath = ctx.currentPath;
 			}
-
 			if (!clip && !compound && (hasFill || hasStroke)) {
 				this._setStyles(ctx);
 				if (hasFill) {
@@ -8699,7 +8698,6 @@ var Color = Base.extend(new function() {
 				var parser;
 				if(name === 'pattern'){
 					parser = componentParsers[type][index] = function(value) {
-						console.log("parser pattern value",value);
 						value = new Pattern(value.item);
 						return value;
 					};
@@ -8762,7 +8760,6 @@ var Color = Base.extend(new function() {
 		_readIndex: true,
 
 		initialize: function Color(arg) {
-			console.log("Initialize Color args", arg);
 			var slice = Array.prototype.slice,
 				args = arguments,
 				read = 0,
@@ -8817,7 +8814,6 @@ var Color = Base.extend(new function() {
 						components.length--;
 					}
 				} else if (argType === 'object') {
-					console.log("arg type is object", arg);
 					if (arg.constructor === Color) {
 						type = arg._type;
 						components = arg._components.slice();
@@ -8833,13 +8829,11 @@ var Color = Base.extend(new function() {
 						type = 'gradient';
 						values = args;
 					} else if (arg.constructor === Pattern) {
-						console.log("Arg constructor - pattern");
 						type = 'pattern';
 						values = args;
 					} else {
 						if('pattern' in arg){
 							type = 'pattern';
-							console.log("pattern in args");
 						}
 						else {
 							type = 'hue' in arg
@@ -9228,7 +9222,10 @@ var Pattern = Base.extend({
 	initialize: function Pattern(item, dontCenter) {
 		this._id = Pattern._id = (Pattern._id || 0) + 1;
 		item.remove();
-		if (item) {
+		if(item._class == 'Raster') {
+			this.item = item;
+			this.raster = item;
+		} else if (item) {
 			this.item = item.clone(false);
 			this.raster = this.item.rasterize();
 		} else {
